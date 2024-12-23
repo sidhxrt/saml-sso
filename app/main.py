d9caef34c9209b9c96cfa1feda7a372f6503be00
sid <154365853+sidhxrt@users.noreply.github.com>
2024-12-24 01:02:47 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from app.routers.saml import router as saml_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Include SAML Router
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(saml_router, prefix="/saml")
 
 @app.get("/")
